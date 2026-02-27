9925d71147424c66914fe74c5b9e225760567d71
dbadhikari <dipuadhikari1819@gmail.com>
2026-02-27 18:54:11 +0545

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./Config/db.js";
+dotenv.config()
+
 
 const app=express()
-
+connectDB()
 app.get("/",(req,res)=>{
     res.send("This is Backend...")
 })
